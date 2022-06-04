6cccd1573e713e04bc62eea147c86539aa4e9ac8
cometbroom <alawi.wow@gmail.com>
2022-06-04 12:08:15 +0200

    Formatting and remove console log

--- week2/prep-exercises/1-generate-random-person-details/src/components/PersonController.js
+++ week2/prep-exercises/1-generate-random-person-details/src/components/PersonController.js
@@ -1,51 +1,51 @@
-import {useState, useEffect} from 'react';
-import Person from './Person';
+import { useState, useEffect } from "react";
+import Person from "./Person";
 
 function PersonController() {
-    const [persons, setPersons] = useState(null);
-    const [onePerson, setOnePerson] = useState(null);
+  const [persons, setPersons] = useState(null);
+  const [onePerson, setOnePerson] = useState(null);
 
+  const getPersons = () => {
+    fetch("https://www.randomuser.me/api?results=10")
+      .then((response) => response.json())
+      .then((data) => {
+        const personsArr = [];
+        data.results.forEach((result) => {
+          const { email, cell, ...rest } = result;
+          const { first, last } = rest.name;
+          personsArr.push({ firstName: first, lastName: last, email, cell });
+        });
+        setPersons(personsArr);
+      })
+      .catch(console.error);
+  };
+  const getOnePerson = (key) => {
+    fetch("https://www.randomuser.me/api?results=1")
+      .then((response) => response.json())
+      .then((data) => {
+        const { email, cell, ...rest } = data.results[0];
+        const { first, last } = rest.name;
+        setOnePerson({ firstName: first, lastName: last, email, cell });
+      })
+      .catch(console.error);
+  };
 
-    const getPersons = () => {
-      fetch("https://www.randomuser.me/api?results=10")
-        .then((response) => response.json())
-        .then((data) => {
-            const personsArr = [];
-            data.results.forEach((result) => {
-                const {email, cell, ...rest} = result;
-                const {first, last} = rest.name;
-                personsArr.push({firstName: first, lastName: last, email, cell});
-                })
-          setPersons(personsArr);
-        })
-        .catch(console.error);
-    }
-    const getOnePerson = (key) => {
-        console.log(key);
-        fetch("https://www.randomuser.me/api?results=1")
-        .then((response) => response.json())
-        .then((data) => {
-                const {email, cell, ...rest} = data.results[0];
-                const {first, last} = rest.name;
-                setOnePerson({firstName: first, lastName: last, email, cell})
-        })
-        .catch(console.error);
-    }
+  useEffect(() => {
+    getPersons();
+  }, []);
 
-    useEffect(() => {
-        getPersons();
-      }, []);
-
-    
-    return (
-        <ul>
-            {       
-                persons !== null && 
-                persons.map((person, idx) => <Person person={person} getPersonHandler={getOnePerson} key={idx}></Person>)
-            }
-        </ul>
-    )
-  
+  return (
+    <ul>
+      {persons !== null &&
+        persons.map((person, idx) => (
+          <Person
+            person={person}
+            getPersonHandler={getOnePerson}
+            key={idx}
+          ></Person>
+        ))}
+    </ul>
+  );
 }
 
 export default PersonController;
